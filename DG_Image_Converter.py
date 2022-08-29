57679ffc89f3c82794c7cf01c6e9f96884bbe9f6
Hydrowelder <dave.a.gable@gmail.com>
2022-08-29 16:33:46 -0400

    Added batch watermarking

--- DG_Image_Converter.py
+++ DG_Image_Converter.py
@@ -1,19 +1,29 @@
 import glob
 from PIL import ImageTk
 import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFont
 import tkinter.filedialog
 from tkinter import *
 from tkinter.messagebox import showerror
 import os
 import multiprocessing
-# import threading
+import sys
+# from rawkit import Cr2
+# import exif
 
+# import threading
 # class windowthread(threading.Thread):
 #     def __init__(self):
 #         threading.Thread.__init__(self)
 #     def run(self):
 #         pass
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+ 
 
 def GetSourceFolder():
     source = tkinter.filedialog.askdirectory()
@@ -45,8 +55,25 @@ def GetDestFolder():
             dest_label.config(text=dest)
 
 
-def ProcessImage(source, dest, file, dest_format):
+def ProcessImage(source, dest, file, dest_format, source_format, watermark):
     im1 = PIL.Image.open(source+'/'+file)
+    if source_format == '.PNG' and dest_format == '.JPG':
+        im1 = im1.convert('RGB')
+         
+    if watermark != "":
+        width, height = im1.width, im1.height
+        
+        font = PIL.ImageFont.truetype("Helvetica.ttf",int(height*0.025))
+        w,h = font.getsize(watermark)
+        
+        w_margin = 0.02
+        h_margin = 0.05
+        
+        draw = PIL.ImageDraw.Draw(im1)
+
+        draw.rectangle(((width-w)-2*w_margin*w, (height-h)-2*h_margin*h, width, height), fill=(0,0,0))
+        draw.text(((width-w)-w_margin*w,(height-h)-h_margin*h),watermark,fill=(255,255,255),font=font)
+    
     im1.save(dest+'/'+os.path.splitext(file)[0]+dest_format)
 
 
@@ -62,8 +89,12 @@ def RunImageConversion():
     files = config["files"]
     source = config["source"]
     dest = config["dest"]
+    watermark = watermark_var.get()
+    
     dest_format = dest_format_var.get()
-
+    source_format = source_format_var.get()
+    
+    
     if source != '' and dest != '':
         run_label.config(text="Now Running")
 
@@ -73,9 +104,11 @@ def RunImageConversion():
                 dest_tuple = [dest]*len(files)
                 files_tuple = files
                 dest_format_tuple = [dest_format]*len(files)
-
+                source_format_tuple = [source_format]*len(files)
+                watermark_tuple = [watermark]*len(files)
+                
                 iter_var = [*zip(source_tuple, dest_tuple,
-                                 files_tuple, dest_format_tuple)]
+                                 files_tuple, dest_format_tuple, source_format_tuple, watermark_tuple)]
 
                 pool = multiprocessing.Pool(int(multiprocessing_var.get()))
                 pool.starmap(ProcessImage, iterable=iter_var)
@@ -94,7 +127,7 @@ def RunImageConversion():
 
         else:
             for file in files:
-                ProcessImage(source, dest, file, dest_format)
+                ProcessImage(source, dest, file, dest_format, source_format, watermark)
             run_label.config(text="Done! Ready for next job.")
 
     else:
@@ -103,11 +136,12 @@ def RunImageConversion():
 
 if __name__ == "__main__":
     config = {"source": '', "dest": '', 'multiprocessing': '1',
-              "source_format": '.CR2', "dest_format": '.PNG', "files": []}
+              "source_format": '.CR2', "dest_format": '.PNG', "files": [], "watermark": u"\u00A9"}
+    
     metrix = {"num_in": 0}
 
-    format_options = [".BLP", ".BMP", ".CR2", ".DDS", ".DIB", ".EPS", ".ICNS",
-                      ".ICO", ".IM", ".JPEG", ".MSP", ".PCX", ".PNG", ".PPM", ".SGI", ".TIFF", ".TIF"]
+    format_options = [".BLP", ".BMP", ".CR2", ".DDS", ".DIB", ".EPS", ".FLIF", ".ICNS",
+                      ".ICO", ".IM", ".JPG", ".MSP", ".PCX", ".PNG", ".PPM", ".SGI", ".TIFF", ".TIF"]
 
     window = Tk()  # init window
     window.geometry("600x400")  # set window size
@@ -120,12 +154,15 @@ if __name__ == "__main__":
     dest_format_var.set(".PNG")
     multiprocessing_var = StringVar(window)
     multiprocessing_var.set("1")
-
+    watermark_var = StringVar(window)
+    watermark_var.set(u"\u00A9")
+    
     window.title("DG Image Converter")
-    window.iconbitmap('DGLogoWhiteBG.ico')
-
+    
+    window.iconbitmap(r'C:\Users\davea\University of Illinois - Urbana\Class Files - Documents\Projects\DG Image Converter\DGLogoWhiteBG.ico')
+    
     img = ImageTk.PhotoImage(PIL.Image.open(
-        "DGLogoBlackBG.png").resize((50, 50)))
+        r"C:\Users\davea\University of Illinois - Urbana\Class Files - Documents\Projects\DG Image Converter\DGLogoBlackBG.ico").resize((50, 50)))
 
     logo_frame = Frame(window, width=2, height=2)
     logo_label = Label(logo_frame, image=img, relief="solid")
@@ -148,6 +185,9 @@ if __name__ == "__main__":
     multiprocessing_frame = Frame(window)
     multiprocessing_frame.config(bg='black', pady=padding)
     multiprocessing_frame.pack()
+    watermark_frame = Frame(window)
+    watermark_frame.config(bg='black', pady=padding)
+    watermark_frame.pack()
     run_frame = Frame(window)
     run_frame.config(bg='black', pady=padding)
     run_frame.pack()
@@ -177,8 +217,16 @@ if __name__ == "__main__":
         multiprocessing_frame, text="Number of Cores (max "+str(os.cpu_count())+"):", bg='black', fg='white')
     multiprocessing_input = Entry(
         multiprocessing_frame, width=2, textvariable=multiprocessing_var)
+    multiprocessing_var.set(1)
+    
+    watermark_label = Label(
+        watermark_frame, text="Watermark:", bg='black', fg='white')
+    watermark_input = Entry(
+        watermark_frame, width=30, textvariable=watermark_var)
+    
     # multiprocessing_input.insert(0,"0")
-
+    watermark_var.set(u"\u00A9 David Gable")
+    
     run_label = Label(run_frame, text="Not Running.", bg='black', fg='white')
     run_button = Button(run_frame, text="Convert Files",
                         fg="black", command=RunImageConversion)
@@ -197,6 +245,9 @@ if __name__ == "__main__":
     multiprocessing_label.grid(row=0, column=0)
     multiprocessing_input.grid(row=0, column=1)
 
+    watermark_label.grid(row=0, column=0)
+    watermark_input.grid(row=0, column=1)
+    
     run_button.grid(row=0, column=0)
     run_label.grid(row=1, column=0)
 
